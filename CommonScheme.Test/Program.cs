7dc1e73b0c8b934a42c828308b12e8e2a9c19a30
wutengjian <wutengjian@163.com>
2020-09-15 20:57:07 +0800

    s

---
+++ CommonScheme.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommonScheme.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
