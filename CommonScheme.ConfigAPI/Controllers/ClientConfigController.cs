eb9513500b88807c1d60c8e706a76e7c53645bc1
wutengjian <wutengjian128@163.com>
2020-11-25 23:41:32 +0800

    客户端api定义，注册、注销 75a9803

---
+++ CommonScheme.ConfigAPI/Controllers/ClientConfigController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommonScheme.ConfigCore.CacheServices;
+using CommonScheme.ConfigCore.ClientServices;
+using CommonScheme.ConfigCore.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommonScheme.ConfigAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ClientConfigController : ControllerBase
+    {
+        [Route("/api/ClientConfig/RegisterClient")]
+        [HttpPost]
+        public void SubscribeConfig(RegistClientModel clientConfig)
+        {
+            if (clientConfig.ID < 1)
+                return;
+            if (clientConfig.ClientState > 0&&clientConfig.Config!=null)
+            {
+                ClientMonitor.RegisterConfig(clientConfig.ID, CacheFactory.MadePrefix(clientConfig.Config.Code, clientConfig.Config.ParentID));
+            }
+            else if (clientConfig.ClientState < 0)
+            {
+                ClientMonitor.CancelConfig(clientConfig.ID);
+            }
+            else if (string.IsNullOrEmpty(clientConfig.PushType) == false)
+            {
+                //更改推送给客户端配置
+            }
+        }
+    }
+}
---
+++ CommonScheme.ConfigAPI/Controllers/OAConfigController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommonScheme.ConfigCore.CacheServices;
+using CommonScheme.ConfigCore.ClientServices;
+using CommonScheme.ConfigCore.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommonScheme.ConfigAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class OAConfigController : ControllerBase
+    {
+        [Route("/api/OAConfig/RegisterClient")]
+        [HttpPost]
+        public void RegisterClient(ClientModel client)
+        {
+            registerConfig(client,null);
+        }
+        [Route("/api/OAConfig/RegisterHttpClient")]
+        [HttpPost]
+        public void RegisterHttpClient(ClientHttpModel client)
+        {
+            registerConfig(client,"Http");
+        }
+        [Route("/api/OAConfig/RegisterRabbitMQClient")]
+        [HttpPost]
+        public void RegisterRabbitMQClient(ClientRabbitMQModel client)
+        {
+            registerConfig(client, "RabbitMQ");
+        }
+        [Route("/api/OAConfig/RegisterWebSocketClient")]
+        [HttpPost]
+        public void RegisterWebSocketClient(ClientWebSocketModel client)
+        {
+            registerConfig(client, "WebSocket");
+        }
+        private void registerConfig(ClientModel client,string pushType)
+        {
+            if (client.ID > 0 && client.ClientState > 0&&string.IsNullOrEmpty(pushType)==false)
+                ClientMonitor.RegisterConfig(client.ID);
+        }
+    }
+}
